6868c29d779bcf299efe2c4874dc8d2d2c7521ae
Juan Diego Peña Ceferino <juandiegopenaceferino@gmail.com>
2025-01-23 11:54:56 -0500

    Carrusel de categories

---
+++ frontend/src/components/CouroselCategory/CategoryCarousel.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import "./CategoryCarousel.css";
+
+export default function CategoryCarousel() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/categories");
+        const data = await response.json();
+        setCategories(data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  return (
+    <>
+      <h1 >Buscar por tipo de alojamiento</h1>
+      <div className="carousel-container">
+        {categories.map((category, index) => (
+          <div
+            key={index}
+            className="carousel-card"
+          >
+            <div className="carousel-card-image">
+              <img
+                src={category.imageUrl || "/placeholder.svg"}
+                alt={category.name}
+              />
+            </div>
+            <div className="carousel-card-content">
+              <h3>{category.name}</h3>
+              <p>#NUMERO</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
--- frontend/src/components/Pages/PagePrincipal.jsx
+++ frontend/src/components/Pages/PagePrincipal.jsx
@@ -1,13 +1,12 @@
 
-import CreateProductForm from '../Forms/CreateProduct/PropertyForm'
 import NavBar from '../NavBar/NavBar'
+import CategoryCarousel from '../CouroselCategory/CategoryCarousel'
 
 export const PagePrincipal = () => {
   return (
     <>
         <NavBar />
-        <h1>CATEGORIAS</h1>
-        <CreateProductForm />
+        <CategoryCarousel />
   
 
 
