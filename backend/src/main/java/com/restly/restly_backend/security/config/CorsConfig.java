0f6a94a12cbbd7d806e18ca36623b0765dab4473
Juan Diego Peña Ceferino <juandiegopenaceferino@gmail.com>
2025-02-04 14:11:08 -0500

    Punto de control backend

--- backend/src/main/java/com/restly/restly_backend/security/config/CorsConfig.java
+++ backend/src/main/java/com/restly/restly_backend/security/config/CorsConfig.java
@@ -2,18 +2,16 @@ package com.restly.restly_backend.security.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5175")
+                .allowedOriginPatterns("http://localhost:5173", "http://localhost:5175")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true);
     }
 }
-
